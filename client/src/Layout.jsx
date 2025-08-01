8e2531868ee1465ad74026c2f14214a75951e655
tufail101 <modreneducation479@gmail.com>
2025-08-02 02:07:33 +0530

    ..

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar/Navbar'
+import Navbar from './component/navbar/Navbar'
 import { Outlet } from 'react-router'
 import Footer from './component/Footer/Footer'
 import ThemeToggle from './component/Theme/ThemeToggle'
