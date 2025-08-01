55af70e6b0e727176c2c975d91a1bd1153f9108c
tufail101 <modreneducation479@gmail.com>
2025-08-02 02:25:56 +0530

    deBugging with file name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,12 @@ import {
   Route,
   RouterProvider,
 } from "react-router";
-import Home from "./component/Home/Home";
+import Home from "./components/Home/Home";
 import Layout from "./Layout";
 import "./App.css";
 import { ThemeProvider } from "./Hooks/ContextApi/Theme/ThemeProvider";
-import Login from "./component/Login/Login";
-import BrowseNotes from "./component/BrowseNotes/BrowseNotes";
+import Login from "./components/Login/Login";
+import BrowseNotes from "./components/BrowseNotes/BrowseNotes";
 
 function App() {
   const router = createBrowserRouter(
