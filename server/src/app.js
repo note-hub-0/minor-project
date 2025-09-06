37b365bffe43e3168b6d4391567b8fa237b5924f
tufail101 <modreneducation479@gmail.com>
2025-09-06 22:34:38 +0530

    ..

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://minor-project-backend-wopb.onrender.com",
     credentials: true
 }))
 
